ce3856f88f66e4483468f280476618fb6ee743ee
Yidong Ren <git@yidong.im>
2016-04-12 15:54:34 -0400

    Refactor

--- hw4/md_multi.hpp
+++ hw4/md_multi.hpp
@@ -6,12 +6,25 @@
 #include <random>
 #include <pthread.h>
 
-#define DIMENSION 2
-
 using std::vector;
 using std::cout;
-
 typedef vector<double> coordinate_vector;
+
+
+namespace MD{
+#define DIMENSION 2
+
+double  LJEplison,      // Eplison in LJ potential equation
+        LJSigma,        // Sigma in LJ potential equation, equilibrium distance
+        Lambda,         // atom velocities factor
+        Mass,           // Mass of each atom
+        BoxSize,        // Half size of simulated box, simulation space is [-BoxSize,BoxSize]
+        Delta,          // Delta to calculate derivative of potential, force, suggest: max_distance/Delta ~ 10^14
+        TimeStep;       // TimeStep of each calculation
+int     NThread,        // Number of threads to spawn, sugguest n_CPU + 1~2
+        TotalSteps,     // Number of TimeSteps to be simulated
+        OutputInterval; // Print output each nth step
+
 
 double potentials(const vector<coordinate_vector> &pos_mat) {
   double pot = 0;
@@ -20,7 +33,9 @@ double potentials(const vector<coordinate_vector> &pos_mat) {
       double dist2 = 0.0;
       for (unsigned int k=0; k<DIMENSION; ++k) {
         double d = pos_mat[i][k]-pos_mat[j][k];
-        dist2 += d*d;
+        if      (d >  BoxSize) d -= 2*BoxSize;
+        else if (d < -BoxSize) d += 2*BoxSize;
+        dist2 += d * d;
       }
       pot += 4*LJEplison * ( pow(LJSigma, 12)/pow(dist2, 6) - pow(LJSigma, 6)/pow(dist2, 3) );
     }
@@ -62,34 +77,27 @@ vector<coordinate_vector> acceleration(const vector<coordinate_vector> &pos_mat,
   for (unsigned int i=0; i<pos_mat.size(); ++i) 
     result_vec[i].resize(DIMENSION);
   
-  // Prepare arguments for each thread
+  // Prepare arguments for each thread and spawn force threads 
+  pthread_t children[NThread];
   unsigned int BatchSize = pos_mat.size()/NThread;
   vector<force_thread_arg_t> force_thread_args(NThread);
-  for (unsigned int i=0; i<force_thread_args.size(); ++i) {
+  for (unsigned int i=0; i<NThread; ++i) {
     force_thread_args[i].pos_mat  = pos_mat;
     force_thread_args[i].orig_pot = orig_pot;
     force_thread_args[i].range_begin = i*BatchSize;
-    force_thread_args[i].range_end = (i+1)*BatchSize;
+    if (i != NThread-1) force_thread_args[i].range_end = (i+1)*BatchSize;
+    else                force_thread_args[i].range_end = pos_mat.size();
     force_thread_args[i].result_vec_p = &result_vec;
-  }
-
-  // Add all leftover atoms to the last thread
-  force_thread_args.back().range_end = pos_mat.size();
-
-  // Spawn threads
-  pthread_t children[NThread];
-  for (unsigned int i=0; i<NThread; ++i) {
     pthread_create(&children[i], NULL, *force_thread, &force_thread_args[i]);
   }
-
-  // Join threads
+  
+  // Join threads and calculate acceleration
   for (unsigned int i=0; i<NThread; ++i) {
     pthread_join(children[i], NULL);
-  }
-  
-  for (unsigned int i=0; i<result_vec.size(); ++i) {
-    for (unsigned int j=0; j<DIMENSION; ++j) {
-      result_vec[i][j] /= mass_mat[i];
+    for (unsigned int j=force_thread_args[i].range_begin; j<force_thread_args[i].range_end; ++j) {
+      for (unsigned int k=0; k<DIMENSION; ++k) {
+        result_vec[j][k] /= mass_mat[j];
+      }
     }
   }
   return result_vec;
@@ -103,9 +111,8 @@ void velocity_verlet(vector<coordinate_vector> &pos_mat,
   for (unsigned int i=0; i<pos_mat.size(); ++i) {
     for (unsigned int j=0; j<DIMENSION; ++j) {
       pos_mat[i][j] += vel_mat[i][j]*TimeStep + accel_mat[i][j]*TimeStep*TimeStep/2;
-      if (abs(pos_mat[i][j]) > BoxSize) {
-        vel_mat[i][j] = -vel_mat[i][j];
-      }
+      if      (pos_mat[i][j] >  BoxSize ) pos_mat[i][j] -= 2*BoxSize;
+      else if (pos_mat[i][j] < -BoxSize ) pos_mat[i][j] += 2*BoxSize;
     }
   }
 
@@ -117,7 +124,7 @@ void velocity_verlet(vector<coordinate_vector> &pos_mat,
       new_half_vel[i].push_back(vel_mat[i][j] + accel_mat[i][j]*TimeStep/2);
     }
   }
-
+  
   //Step 3: new_vel_mat = vel_mat + (accel_mat+new_accel_mat)*TimeStep/2
   vector<coordinate_vector> new_accel_mat = acceleration(pos_mat, mass_mat);
   for (unsigned int i=0; i<vel_mat.size(); ++i) {
@@ -147,7 +154,7 @@ void print_pos(const vector<coordinate_vector> &pos_mat,
                double energy,
                unsigned int step) {
   cout <<pos_mat.size() <<'\n'
-       <<"Step: " <<step <<" Energy: " <<energy <<'\n';
+       <<"Time " <<step*TimeStep <<"\tEnerygy " <<energy <<'\n';
   for (unsigned int i=0; i<pos_mat.size(); ++i) {
     cout <<'A';
     for (unsigned int j=0; j<DIMENSION; ++j) {
@@ -157,3 +164,16 @@ void print_pos(const vector<coordinate_vector> &pos_mat,
   }
 }
 
+void simulate(vector<coordinate_vector> &pos_mat,
+              vector<coordinate_vector> &vel_mat,
+              vector<double>            &mass_mat) {
+  print_pos(pos_mat, energy(pos_mat, vel_mat, mass_mat), 0);
+  for (unsigned int i=1; i<=TotalSteps; ++i) {
+    velocity_verlet(pos_mat, vel_mat, mass_mat);
+    if (i%OutputInterval==0) {
+      print_pos(pos_mat, energy(pos_mat, vel_mat, mass_mat), i);
+    }
+  }      
+}
+
+}
