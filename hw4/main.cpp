3b006b6c8a73ab06b300f7a885ed3b94e15b1d1a
Yidong Ren <ryd994@163.com>
2016-05-06 18:03:25 -0400

    Feature: Introduce atom type.

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -4,40 +4,40 @@
 int main() {
   // Parameters. For usage see "md_multi.hpp"
   MD::NThread         =2;
-  MD::LJEplison       =1.0e-1;
-  MD::LJSigma         =1.0;
-  MD::BoxSize         =15.0;
+  MD::LJEplison[0][0] =1.0e-1;
+  MD::LJSigma[0][0]   =1.0;
+  MD::BoxSize         =10.0;
   MD::Delta           =1.0e-12;
-  MD::TimeStep        =1e-5;
-  MD::TotalSteps      =100;
+  MD::TimeStep        =3e-4;
+  MD::TotalSteps      =1000;
   MD::OutputInterval  =10;
-  
+  MD::BoundaryConditions.fill(MD::boundary_condition_t::Periodic);
+  MD::Mass[0]         =1.0;
   
   std::default_random_engine rng;
   std::normal_distribution<double> gasdev;
-  vector<coordinate_vector> my_pos;
-  vector<coordinate_vector> my_vel;
-  vector<double> my_mass;
+  vector<coordinate_vector> my_pos_mat;
+  vector<coordinate_vector> my_vel_mat;
+  vector<atom_type_t>       my_type_vec;
   
-  double mass   = 1.0;
   double Lambda = 20;
-  int n = 6;
+  int n = 4;
   for (int i=-n;i<n;++i) {
     for (int j=-n;j<n;++j) {
       coordinate_vector p;
       p[0] = i*MD::BoxSize/(n+0.5);
       p[1] = j*MD::BoxSize/(n+0.5);
-      my_pos.push_back(p);
+      my_pos_mat.push_back(p);
       
       coordinate_vector v;
       v[0]=gasdev(rng)*Lambda;
       v[1]=gasdev(rng)*Lambda;
-      my_vel.push_back(v);
+      my_vel_mat.push_back(v);
       
-      my_mass.push_back(mass);
+      my_type_vec.push_back(0);
     }
   }
   
   // Run simulation
-  MD::simulate(my_pos, my_vel, my_mass);
+  MD::simulate(my_pos_mat, my_vel_mat, my_type_vec);
 }
--- hw4/md_multi.hpp
+++ hw4/md_multi.hpp
@@ -9,20 +9,19 @@
 
 
 #define DIMENSION 2
+#define MAX_TYPES 5
 
 using std::vector;
 using std::cout;
 using std::array;
 typedef array<double, DIMENSION> coordinate_vector;
 
+typedef unsigned int atom_type_t;
+
 
 namespace MD {
 
-double  LJEplison,                      // Eplison in LJ potential equation
-        LJSigma,                        // Sigma in LJ potential equation, equilibrium distance
-        Lambda,                         // atom velocities factor
-        Mass,                           // Mass of each atom
-        BoxSize,                        // Half size of simulated box, simulation space is [-BoxSize,BoxSize]
+double  BoxSize,                        // Half size of simulated box, simulation space is [-BoxSize,BoxSize]
         Delta,                          // Delta to calculate derivative of potential, force, suggest: max_distance/Delta ~ 10^14
         TimeStep;                       // TimeStep of each calculation
 unsigned int  NThread,                  // Number of threads to spawn, sugguest n_CPU + 1~2
@@ -30,10 +29,14 @@ unsigned int  NThread,                  // Number of threads to spawn, sugguest 
               OutputInterval;           // Print output each nth step
 
 enum class boundary_condition_t {Periodic, Reflective};
-
 array<boundary_condition_t, DIMENSION> BoundaryConditions;
 
-double potentials(const vector<coordinate_vector> &pos_mat) {
+array<array<double, MAX_TYPES>, MAX_TYPES> LJEplison;
+array<array<double, MAX_TYPES>, MAX_TYPES> LJSigma;
+array<double, MAX_TYPES> Mass;
+
+
+double potentials(const vector<coordinate_vector> &pos_mat, const vector<atom_type_t> &type_vec) {
   double pot = 0;
   for (size_t i=0; i < pos_mat.size(); ++i) {
     for (size_t j=i+1; j < pos_mat.size(); ++j) {
@@ -46,7 +49,10 @@ double potentials(const vector<coordinate_vector> &pos_mat) {
         }
         dist2 += d * d;
       }
-      pot += 4*LJEplison * ( pow(LJSigma*LJSigma/dist2, 6) - pow(LJSigma*LJSigma/dist2, 3) );
+      
+      atom_type_t i_type = type_vec[i],
+                  j_type = type_vec[j];
+      pot += 4*LJEplison[i_type][j_type] * ( pow(LJSigma[i_type][j_type]*LJSigma[i_type][j_type]/dist2, 6) - pow(LJSigma[i_type][j_type]*LJSigma[i_type][j_type]/dist2, 3) );
     }
   }
   return pot;
@@ -54,6 +60,7 @@ double potentials(const vector<coordinate_vector> &pos_mat) {
 
 struct force_thread_arg_t {
   vector<coordinate_vector> pos_mat;
+  const vector<atom_type_t> *type_vec_p;
   double orig_pot;
   size_t range_begin, range_end;
   vector<coordinate_vector> *result_vec_p;
@@ -64,12 +71,13 @@ void* force_thread(void *args_p) {
   vector<coordinate_vector> &pos_mat = args->pos_mat;
   double orig_pot(args->orig_pot);
   vector<coordinate_vector> &result_vec = *(args->result_vec_p);
+  const vector<atom_type_t> &type_vec = *(args->type_vec_p);
 
   for (size_t i=args->range_begin; i<args->range_end; ++i) {
     for (unsigned int j=0; j<DIMENSION; ++j) {
       double orig_pos = pos_mat[i][j];
       pos_mat[i][j] += Delta;
-      double new_pot = potentials(pos_mat);
+      double new_pot = potentials(pos_mat, type_vec);
       pos_mat[i][j]  = orig_pos;
       result_vec[i][j] = (orig_pot-new_pot)/Delta;
     }
@@ -78,8 +86,8 @@ void* force_thread(void *args_p) {
 }
 
 vector<coordinate_vector> acceleration(const vector<coordinate_vector> &pos_mat,
-                                       const vector<double> &mass_mat) {
-  double orig_pot = potentials(pos_mat);
+                                       const vector<atom_type_t> &type_vec) {
+  double orig_pot = potentials(pos_mat, type_vec);
 
   // Allocate result space
   vector<coordinate_vector> result_vec(pos_mat.size());
@@ -95,6 +103,7 @@ vector<coordinate_vector> acceleration(const vector<coordinate_vector> &pos_mat,
     if (i != NThread-1) force_thread_args[i].range_end = (i+1)*BatchSize;
     else                force_thread_args[i].range_end = pos_mat.size();
     force_thread_args[i].result_vec_p = &result_vec;
+    force_thread_args[i].type_vec_p   = &type_vec;
     pthread_create(&children[i], NULL, *force_thread, &force_thread_args[i]);
   }
   
@@ -103,7 +112,7 @@ vector<coordinate_vector> acceleration(const vector<coordinate_vector> &pos_mat,
     pthread_join(children[i], NULL);
     for (size_t j=force_thread_args[i].range_begin; j<force_thread_args[i].range_end; ++j) {
       for (unsigned int k=0; k<DIMENSION; ++k) {
-        result_vec[j][k] /= mass_mat[j];
+        result_vec[j][k] /= Mass[type_vec[j]];
       }
     }
   }
@@ -112,9 +121,9 @@ vector<coordinate_vector> acceleration(const vector<coordinate_vector> &pos_mat,
 
 void velocity_verlet(vector<coordinate_vector> &pos_mat,
                      vector<coordinate_vector> &vel_mat,
-                     const vector<double>      &mass_mat) {
+                     const vector<atom_type_t> &type_vec) {
   //Step 1: pos_mat += vel_mat*TimeStep + accel_mat*TimeStep*TimeStep/2
-  vector<coordinate_vector> accel_mat = acceleration(pos_mat, mass_mat);
+  vector<coordinate_vector> accel_mat = acceleration(pos_mat, type_vec);
   for (size_t i=0; i<pos_mat.size(); ++i) {
     for (unsigned int j=0; j<DIMENSION; ++j) {
       pos_mat[i][j] += vel_mat[i][j]*TimeStep + accel_mat[i][j]*TimeStep*TimeStep/2;
@@ -134,7 +143,7 @@ void velocity_verlet(vector<coordinate_vector> &pos_mat,
   }
   
   //Step 3: new_vel_mat = vel_mat + (accel_mat+new_accel_mat)*TimeStep/2
-  vector<coordinate_vector> new_accel_mat = acceleration(pos_mat, mass_mat);
+  vector<coordinate_vector> new_accel_mat = acceleration(pos_mat, type_vec);
   for (size_t i=0; i<vel_mat.size(); ++i) {
     for (unsigned int j=0; j<DIMENSION; ++j) {
       vel_mat[i][j] += (accel_mat[i][j]+new_accel_mat[i][j])*TimeStep/2;
@@ -147,17 +156,17 @@ void velocity_verlet(vector<coordinate_vector> &pos_mat,
 
 double energy(const vector<coordinate_vector> &pos_mat,
               const vector<coordinate_vector> &vel_mat,
-              const vector<double>            &mass_mat) {
+              const vector<atom_type_t>       &type_vec) {
   double ener = 0;
   for (size_t i=0; i<vel_mat.size(); ++i) {
     double v2 = 0;
     for (unsigned int j=0; j<DIMENSION; ++j) {
       v2 += vel_mat[i][j]*vel_mat[i][j];
     }
-    ener += mass_mat[i]*v2;
+    ener += Mass[type_vec[i]]*v2;
   }
   ener /= 2;
-  ener += potentials(pos_mat);
+  ener += potentials(pos_mat, type_vec);
   return ener;
 }
 
@@ -177,12 +186,12 @@ void print_pos(const vector<coordinate_vector> &pos_mat,
 
 void simulate(vector<coordinate_vector> &pos_mat,
               vector<coordinate_vector> &vel_mat,
-              vector<double>            &mass_mat) {
-  print_pos(pos_mat, energy(pos_mat, vel_mat, mass_mat), 0);
+              const vector<atom_type_t> &type_vec) {
+  print_pos(pos_mat, energy(pos_mat, vel_mat, type_vec), 0);
   for (size_t i=1; i<=TotalSteps; ++i) {
-    velocity_verlet(pos_mat, vel_mat, mass_mat);
+    velocity_verlet(pos_mat, vel_mat, type_vec);
     if (i%OutputInterval==0) {
-      print_pos(pos_mat, energy(pos_mat, vel_mat, mass_mat), i);
+      print_pos(pos_mat, energy(pos_mat, vel_mat, type_vec), i);
     }
   }      
 }
