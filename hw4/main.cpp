c780b7e74fe51371802c3302a2379ae31407f114
Yidong Ren <ryd994@163.com>
2016-05-09 18:04:59 -0400

    Archive for MTLE4500 Final Project

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -1,18 +1,36 @@
 #define NDEBUG
 #include "md_multi.hpp"
+#include "param.hpp"
+#include <cmath>
+#define LJMIN 1.122462
+
 
 int main() {
   // Parameters. For usage see "md_multi.hpp"
-  MD::NThread         =4;
-  MD::LJEplison[0][0] =1.0e-1;
-  MD::LJSigma[0][0]   =1.0;
-  MD::BoxSize         ={5, 10};
+  MD::NThread         =12;
+  MD::BoxSize[0]      =10.0;
+  MD::BoxSize[1]      =20.0;
   MD::Delta           =1.0e-12;
-  MD::TimeStep        =3e-4;
-  MD::TotalSteps      =1000;
-  MD::OutputInterval  =10;
-  MD::BoundaryConditions.fill(MD::boundary_condition_t::Periodic);
-  MD::Mass[0]         =1.0;
+  MD::TimeStep        =1e-3;
+  MD::TotalSteps      =500000;
+  MD::OutputInterval  =100;
+  MD::BoundaryConditions[0]=MD::boundary_condition_t::Periodic;
+  MD::BoundaryConditions[1]=MD::boundary_condition_t::Reflective;
+  MD::Cooling         = 1.0 - 2.0e-3;
+  MD::Mass            = {1,1,1};
+  MD::Fixed           = {1,0,0};
+  for (int i=0; i<2; ++i) {
+    for (int j=0; j<2; ++j) {
+      MD::LJEplison[i][j] = EP1;
+      MD::LJSigma[i][j]   = SI1;
+    }
+  }
+  for (int i=0; i<2; ++i) {
+    MD::LJEplison[i][2] = MD::LJEplison[2][i] = EP2;
+    MD::LJSigma[i][2]   = MD::LJSigma[2][i]   = SI2;
+  }
+  MD::LJEplison[2][2] = EP3;
+  MD::LJSigma[2][2]   = SI3;
   
   std::default_random_engine rng;
   std::normal_distribution<double> gasdev;
@@ -20,23 +38,51 @@ int main() {
   vector<coordinate_vector> my_vel_mat;
   vector<atom_type_t>       my_type_vec;
   
-  double Lambda = 20;
-  int n = 4;
-  for (int i=-n;i<n;++i) {
-    for (int j=-n;j<n;++j) {
+  int n[2] = {5,10};
+  for (int i=-n[0];i<n[0];++i) {
+    for (int j=0;j<n[1];++j) {
       coordinate_vector p;
-      p[0] = i*MD::BoxSize[0]/n;
-      p[1] = j*MD::BoxSize[1]/n;
+      p[0] = i*MD::BoxSize[0]/(n[0]+0.5);
+      p[1] = j*MD::BoxSize[1]/(n[1]+0.5);
       my_pos_mat.push_back(p);
       
       coordinate_vector v;
-      v[0]=gasdev(rng)*Lambda;
-      v[1]=gasdev(rng)*Lambda;
+      v[0]=gasdev(rng)*LAMBDA;
+      v[1]=gasdev(rng)*LAMBDA;
       my_vel_mat.push_back(v);
       
-      my_type_vec.push_back(0);
+      my_type_vec.push_back(2);
     }
   }
+  
+  n[0] = MD::BoxSize[0]/LJMIN/MD::LJSigma[0][0];
+  for (int i=-n[0];i<n[0];i++) {
+          coordinate_vector p;
+          p[0] = i*MD::LJSigma[0][0]*LJMIN;
+          p[1] = -MD::BoxSize[1]+1*MD::LJSigma[0][0];
+          my_pos_mat.push_back(p);
+          p[1] = -MD::BoxSize[1]+3*MD::LJSigma[0][0];
+          my_pos_mat.push_back(p);
+          p[0] = (i+0.5)*MD::LJSigma[0][0]*LJMIN;
+          p[1] = -MD::BoxSize[1]+2*MD::LJSigma[0][0];
+          my_pos_mat.push_back(p);
+          p[1] = -MD::BoxSize[1]+4*MD::LJSigma[0][0];
+          my_pos_mat.push_back(p);
+          
+          coordinate_vector v;
+          v[0]=0;
+          v[1]=0;
+          my_vel_mat.push_back(v);
+          my_vel_mat.push_back(v);
+          my_vel_mat.push_back(v);
+          my_vel_mat.push_back(v);
+          
+          my_type_vec.push_back(0);
+          my_type_vec.push_back(1);
+          my_type_vec.push_back(0);
+          my_type_vec.push_back(1);
+  }
+
   
   // Run simulation
   MD::simulate(my_pos_mat, my_vel_mat, my_type_vec);
